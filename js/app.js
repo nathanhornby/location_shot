b4620eb26ec6fd85201b8eba88c8025b1a9f82f1
Nathan Hornby <nathan@3degrees>
2014-08-23 12:43:43 +0100

    Added fail condition for initial search.

--- js/app.js
+++ js/app.js
@@ -21,30 +21,36 @@ $(document).ready(function(){
 				url: "data.php"
 			})
 			.done(function(data){
-				var result = $.parseJSON(data);
+				if(data){
+					var result = $.parseJSON(data);
 
-				// Update location, time and weather
-				$("#location_search input[name='location']").val('');
-				$("#location_search input[name='location']").attr('placeholder',result.location);
-				$("#location_meta").html('<p>'+result.weather.time+' <img id="location_weather" src="img/climacons/'+result.weather.icon+'.svg" /></p>');
+					// Update location, time and weather
+					$("#location_search input[name='location']").val('');
+					$("#location_search input[name='location']").attr('placeholder',result.location);
+					$("#location_meta").html('<p>'+result.weather.time+' <img id="location_weather" src="img/climacons/'+result.weather.icon+'.svg" /></p>');
 
-				// Update map
-				$("#location_map").attr('style','background-image:url('+result.map+'&style=visibility:off&style=visibility:off&style=feature:water|visibility:on|hue:0x88c8ea|saturation:-10|lightness:1&style=feature:landscape|visibility:on|hue:0xf3f4f4|saturation:-100|lightness:1&style=feature:poi.park|visibility:on|hue:0x91c9ae|saturation:10|lightness:1&style=feature:road|visibility:on&style=feature:road.arterial|visibility:on|hue:0xfddf84|saturation:100|lightness:1&style=feature:road.highway|visibility:on|hue:0xfddf84|saturation:100|lightness:1&style=feature:road.local|visibility:on|hue:0xffffff|saturation:-100|lightness:100&style=feature:road|element:geometry.stroke|visibility:off&style=element:labels|visibility:off);');
+					// Update map
+					$("#location_map").attr('style','background-image:url('+result.map+'&style=visibility:off&style=visibility:off&style=feature:water|visibility:on|hue:0x88c8ea|saturation:-10|lightness:1&style=feature:landscape|visibility:on|hue:0xf3f4f4|saturation:-100|lightness:1&style=feature:poi.park|visibility:on|hue:0x91c9ae|saturation:10|lightness:1&style=feature:road|visibility:on&style=feature:road.arterial|visibility:on|hue:0xfddf84|saturation:100|lightness:1&style=feature:road.highway|visibility:on|hue:0xfddf84|saturation:100|lightness:1&style=feature:road.local|visibility:on|hue:0xffffff|saturation:-100|lightness:100&style=feature:road|element:geometry.stroke|visibility:off&style=element:labels|visibility:off);');
 
-				// Insert images
-				if(result.images){
-					$.each(result.images,function(index,value){
-						$("#location_images").append('<div class="image"><span>'+value.title+'</span><img src="'+value.image+'"" /></div>');
-					});
-					$("#location_images").append('<div class="clear"></div>');
+					// Insert images
+					if(result.images){
+						$.each(result.images,function(index,value){
+							$("#location_images").append('<div class="image"><span>'+value.title+'</span><img src="'+value.image+'"" /></div>');
+						});
+						$("#location_images").append('<div class="clear"></div>');
+					}else{
+						$("#location_images").append('<p>There are no images for this location</p>');
+					}
+					
+					// Load it all in
+					$("#location_bar").css('top','0');
+					$("#location_map").css('top','0');
+					$("#location_images").delay(600).fadeIn();
 				}else{
-					$("#location_images").append('<p>There are no images for this location</p>');
+					$("#location_search input[name='location']").val('');
+					$("#location_search input[name='location']").attr('placeholder','Sorry, no results!');
+					$("#location_bar").css('top','0');
 				}
-				
-				// Load it all in
-				$("#location_bar").css('top','0');
-				$("#location_map").css('top','0');
-				$("#location_images").delay(600).fadeIn();
 			})
 			.fail(function(){
 				// Blurt
